24e3160cdab7abb7e974c960cf573f40f8e802ff
AlirezaDizaji <alirezadizaji@yahoo.com>
2020-12-24 19:24:27 +0330

    basic Tests added

--- django_project/users/tests.py
+++ django_project/users/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
 # Create your tests here.
+from .models import Profile
+
+
+class ProfileTestCase(TestCase):
+    def test_login(self):
+        # user1 = Profile.objects.get(pk=1)
+        self.assertEqual(3, 3)
